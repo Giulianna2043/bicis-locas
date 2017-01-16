9c3b5d91e100b2df9a3047c6305729cf635eb8e9
giulianna <j.bustinza42@gmail.com>
2017-01-16 16:37:00 -0500

    subiendo final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,154 @@
-function validateForm(){
-	/* Escribe tú código aquí */
+//validacion de nombres
+function validacionNom(event){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        var nombre = document.getElementById("name").value;
+   
+        
+        var nombreArray = nombre.split("");
+        var primeraLetra = nombreArray[0];
+        var primeraMayus = primeraLetra.toUpperCase();
+        var cortePalabra = false;
+      
+        for(var i=1;i<nombreArray.length;i++){
+            if(cortePalabra)
+            {    
+                primeraMayus += nombreArray[i].toUpperCase();
+                cortePalabra = false;
+            }
+            else
+                primeraMayus+=nombreArray[i];
+            if(nombreArray[i] == " ")
+                cortePalabra = true;
+            
+        }
+       
+       document.getElementById("name").value = primeraMayus; 
 }
+
+
+function validacionNomb(event){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        palabra = window.event.keyCode;
+
+        if((palabra>=65 && palabra<=90)||palabra==8||palabra==32){
+         
+        } 
+        else 
+        {
+            // = "";
+            //alert("Este campo solo permite caracteres de la A-Z");
+            event.preventDefault();
+        }
+      
+} 
+
+//validacion de apellidos
+
+function validateApe(event){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        var apellido = document.getElementById("lastname").value;
+   
+        
+        var apellidoArray = apellido.split("");
+        var primeraLetra = apellidoArray[0];
+        var primeraMayus = primeraLetra.toUpperCase();
+        var cortePalabra = false;
+      
+        for(var i=1;i<apellidoArray.length;i++){
+            if(cortePalabra)
+            {    
+                primeraMayus += apellidoArray[i].toUpperCase();
+                cortePalabra = false;
+            }
+            else
+                primeraMayus+=apellidoArray[i];
+            if(apellidoArray[i] == " ")
+                cortePalabra = true;
+            
+        }
+       
+       document.getElementById("lastname").value = primeraMayus; 
+}
+
+
+function validateApellido(event){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        palabra = window.event.keyCode;
+
+        if((palabra>=65 && palabra<=90)||palabra==8||palabra==32){
+         
+        } 
+        else 
+        {
+            // = "";
+            //alert("Este campo solo permite caracteres de la A-Z");
+            event.preventDefault();
+        }
+      
+} 
+
+//validacion de correo 
+
+function validacionEmail(event){
+   
+    var email = document.getElementById("input-email").value;
+    var salida=document.getElementById("salida");
+   
+        if (!/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(email))
+        {    
+            alert("email invalido");
+        }
+}
+
+//validacion de contraseña 
+
+function validacionPassword(event){
+   
+    var password = document.getElementById("input-password").value;
+
+    //El campo password debe tener al menos 6 caracteres
+//El campo password no puede ser igual a "password" ó "123456" ó "098754"
+    
+        if(password == "123456" || password == "098754" && password.length<6)
+            {
+                alert("contraseña incorrecta ");
+            }           
+}
+
+//validacion de optiones
+
+
+function validateOption(event){
+    var tipo= document.querySelector("select").value;
+    var salida=document.getElementById("salida");
+    
+    if(tipo == 0)
+    {
+       alert("escoge una option")
+    }
+}
+
+function validateForm(){
+    validateOption();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
